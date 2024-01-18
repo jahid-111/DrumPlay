b8393bfe93e410d93dd165726abab00f4b9529e0
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-01-18 03:05:48 +0300

    Demo

--- file/app.js
+++ file/app.js
@@ -8,20 +8,27 @@ window.addEventListener('keydown', function(e){
     // console.log(e.keyCode)
 
     const audio = document.querySelectorAll(`audio[data-key="${e.keyCode}"`)
+        const keys = document.querySelectorAll(`.key[data-key="${e.keyCode}"`)
+        
+        for (const onStyle of keys) {
+            onStyle.style.transition = "background-color 0.s ease-in-out";
+            onStyle.style.backgroundColor = "black";
+        }
     
-    
+        setTimeout(function() {
+            for (const onStyle of keys) {
+                onStyle.style.transition = "";  
+                onStyle.style.backgroundColor = "";
+            }
+        }, 200);
+
+  
     for(const audios of audio ){
-        console.log(audios)
-
-
         if(!audios) return;
         audios.currentTime = 0;
         audios.play()
-
     }
 
-
- 
     
 })
   
